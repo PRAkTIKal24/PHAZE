1199eab1775a75ba0b3270c2497b8681551a29b1
Pratik Jawahar <55033105+PRAkTIKal24@users.noreply.github.com>
2025-10-08 18:37:17 +0000

    prepping plotting suite integration

---
+++ phaze/src/plotting/plotters/__init__.py
@@ -0,0 +1,12 @@
+"""PHAZE Component Plotters.
+
+This module contains specialized plotters for different PHAZE components.
+"""
+
+from .fingerprint_plotter import FingerprintPlotter
+from .zkml_plotter import ZKMLPlotter
+
+__all__ = [
+    "FingerprintPlotter", 
+    "ZKMLPlotter",
+]
